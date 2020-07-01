cdd3a455ca5f2d63531ed01f314714300efcbb7a
Nicolás Salas <nicolas.salas@imfd.cl>
2020-07-01 00:38:02 -0400

    feat: configure nprogress

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const NProgress = require('nprogress')
+
+NProgress.configure({
+  showSpinner: false,
+  trickleRate: 0.2,
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
