b6236e4abdddd442f0e32b820b88fd30c0980dcf
Nicolás Salas <nicolas.salas@imfd.cl>
2020-07-12 02:20:08 -0400

    chore: remove remnants of tailwind

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const tailwind = require('tailwindcss')
-
-module.exports = {
-  plugins: [tailwind('./tailwind.config.js'), require('autoprefixer')],
-}
